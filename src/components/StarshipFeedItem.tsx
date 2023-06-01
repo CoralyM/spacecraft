be2ab62345d509107cf51541b3bfc23e81a01d9d
CoralyM <coraly.mgk@gmail.com>
2023-06-01 12:36:39 +0200

    correction exo 4

--- src/components/StarshipFeedItem.tsx
+++ src/components/StarshipFeedItem.tsx
@@ -9,14 +9,14 @@ const StarshipFeedItem = ({item}: any) => {
 
     return (
         <Card style={styles.container}>
-        <Card.Title title={item.name}/>
-        <Card.Content>
-            <Text>Model: {item.model}</Text>
-            <Text>Crew: {item.crew}</Text>
-            <Text>Hyperdriving Rating: {item.hyperdriving_rating}</Text>
-            <Text>Cost: {item.cost_in_credits}</Text>
-        </Card.Content>
-        <Card.Cover source={image}/>
+            <Card.Cover source={image}/>
+            <Card.Title title={item.name}/>
+            <Card.Content>
+                <Text>Model: {item.model}</Text>
+                <Text>Crew: {item.crew}</Text>
+                <Text>Hyperdriving Rating: {item.hyperdriving_rating}</Text>
+                <Text>Cost: {item.cost_in_credits}</Text>
+            </Card.Content>
         </Card>
     )
 }
