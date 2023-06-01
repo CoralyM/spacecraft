f0a6ed421c6d0421903a58044346e3a8b859fe3a
CoralyM <coraly.mgk@gmail.com>
2023-06-01 15:56:18 +0200

    data exo 2

--- App.tsx
+++ App.tsx
@@ -6,16 +6,22 @@ import LoginScreen from './src/screens/LoginScreen';
 import { TermsScreen } from './src/screens/TermsScreen';
 import { ScrollView } from 'react-native';
 import { StarshipFeedScreen } from './src/screens/StarshipFeedScreen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const App = () => {
+
+  const queryClient = new QueryClient()
+
   return (
     <PaperProvider>
-      {/*<ScrollView>
-        <LoginScreen />
-        <TermsScreen />
-      </ScrollView>
-      */}
-    <StarshipFeedScreen/>
+      <QueryClientProvider client={queryClient}>
+        {/*<ScrollView>
+                  <LoginScreen />
+                  <TermsScreen />
+                </ScrollView>
+                */}
+        <StarshipFeedScreen/>
+      </QueryClientProvider >
     </PaperProvider>
   );
 };
--- src/screens/StarshipFeedScreen.tsx
+++ src/screens/StarshipFeedScreen.tsx
@@ -1,20 +1,60 @@
-import React from "react";
-import { StyleSheet, StatusBar, View, Text, FlatList } from "react-native";
-
+import React, { useEffect } from "react";
+import { StyleSheet, StatusBar, FlatList, RefreshControl } from "react-native";
+import { Text } from 'react-native-paper';
 import { default as data } from "../../api/data.json";
 import StarshipFeedItem from "../components/StarshipFeedItem";
 import ScreenContainer from "../components/ScreenContainer";
+import { useStarships } from "../hooks/UseStarships";
+import {
+    Placeholder,
+    PlaceholderMedia,
+    PlaceholderLine,
+    Fade
+  } from "rn-placeholder";
 
 export const StarshipFeedScreen = () => {
-  return (
-    <ScreenContainer additionalStyle={styles.container}>
-        <FlatList
-            data={data.results}
-            renderItem={({item, index}) => <StarshipFeedItem key={item.name + '-' + index} item={item}/>}
-            keyExtractor={(item, index) => item.name + '-' + index}
-        />
-    </ScreenContainer>
-  );
+
+    const starships = useStarships();
+
+    if (starships.status === 'loading') {
+        return (
+            <ScreenContainer additionalStyle={{alignItems: 'center', justifyContent: 'center', padding: 20}}>
+                <Placeholder Animation={Fade}>
+                    <PlaceholderMedia style={{width: '100%', height: 250, marginBottom: 20}} />
+                    <PlaceholderLine width={80} />
+                    <PlaceholderLine width={50} />
+                    <PlaceholderLine width={40} />
+                    <PlaceholderLine width={30} />
+                </Placeholder>
+                <Placeholder Animation={Fade}>
+                    <PlaceholderMedia style={{width: '100%', height: 250, marginBottom: 20}} />
+                    <PlaceholderLine width={80} />
+                    <PlaceholderLine width={50} />
+                    <PlaceholderLine width={40} />
+                    <PlaceholderLine width={30} />
+                </Placeholder>
+            </ScreenContainer>
+        )
+    } else if (starships.status === 'error') {
+        return (
+            <ScreenContainer additionalStyle={{alignItems: 'center', justifyContent: 'center'}}>
+                <Text variant="headlineMedium">Error loading ships</Text>
+            </ScreenContainer>
+        )
+    }
+
+    return (
+        <ScreenContainer additionalStyle={styles.container}>
+            <FlatList
+                data={data.results}
+                renderItem={({item, index}) => <StarshipFeedItem key={item.name + '-' + index} item={item}/>}
+                keyExtractor={(item, index) => item.name + '-' + index}
+                refreshControl={
+                    <RefreshControl refreshing={starships.isFetching} onRefresh={starships.refetch}/>
+                }
+            />
+        </ScreenContainer>
+    );
 };
 
 const styles = StyleSheet.create({
